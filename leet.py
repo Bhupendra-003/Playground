8cf289a232a78babe5b19e1b513c8df3408b3c11
Bhupendra <bhupendra.raj003.github@gmail.com>
2025-06-13 14:53:26 +0530

    Missing Numeer leet

--- leet.py
+++ leet.py
@@ -1,5 +1,11 @@
-nums1 = [1, 2, 3, 4, 5]
-nums2 = [1, 2, 7]
-nums1 = set(nums1)
-nums3 = nums1.union(nums2)
-print(nums3)
+class Node:
+    def __init__(self, data):
+        self.left = None
+        self.right = None
+        self.data = data
+
+class Tree:
+    def __init__(self, data = 0):
+        self.root = Node(data)
+        
+    
